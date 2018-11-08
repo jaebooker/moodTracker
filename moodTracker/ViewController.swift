79f4072efe798cb91640a27bd2190c528accf14d
jaebooker <jaesonbooker@gmail.com>
2018-11-08 14:47:43 -0800

    Initial Commit

---
+++ moodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  moodTracker
+//
+//  Created by Jaeson Booker on 11/8/18.
+//  Copyright © 2018 Jaeson Booker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
